8e0df85856895a5ad7c77a3e6cc2c837ee305b29
ss473901 <satoshi.473901.1205@gmail.com>
2021-12-11 19:58:14 +0900

    Add

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-alert('Hello');
+// alert('Hello');
+
+// //
+// //
+// //while文
+
+// let state = true;
+
+// while(state){
+//   //0~9の乱数を生成する
+//   let random = Math.floor(Math.random() * 10);
+
+//   //数値が3になったら繰り返し処理を終了する
+//   if(random === 3) state = false;
+
+//   console.log(random);
+
+// }
+
+
+//
+// //
+// //for文
+// for ( let i = 0; i<5; i++ ){
+//   console.log(i);
+// }
+
+
+//
+//
+//break文 forの場合
+// // for ( let i = 0; i<5; i++){
+
+// //   console.log(i);
+
+// //   if ( i === 3)break;
+
+// // }
+
+
+// //break whileの場合
+// let state = true;
+
+// while(state){
+//   let random = Math.floor(Math.random() * 10);
+
+//   if(random === 3) break;
+
+//   console.log(random);
+// }
+
+
+
+//
+//
+//continue文
+for ( let i = 0; i<10; i++)
